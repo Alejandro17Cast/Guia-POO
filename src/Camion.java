e21342746de8f5ab6e455a134a2b04f95014cb20
Alejandro17Cast <luisalejandrocasta3@gmail.com>
2024-05-26 11:23:45 -0600

    camion

--- src/Camion.java
+++ src/Camion.java
@@ -24,4 +24,16 @@ public class Camion extends vehiculoCarga {
 
     }
 
+    @Override
+    public void realizarMantenmiento(){
+        super.realizarMantenmiento();
+        System.out.println("1. Limpieza del motor");
+        System.out.println("2. Cambio de llantas");
+        System.out.println("3. Arreglos de tricos ");
+    }
+    public void costoMantenimiento(double kmrecorridos, double costoporkm){
+        double CostoMantenimiento=kmrecorridos*costoporkm; 
+        System.out.println("El costo total por el mantenimiento es: "+CostoMantenimiento);
+        
+          }
 }
