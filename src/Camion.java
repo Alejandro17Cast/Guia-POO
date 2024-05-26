c88860566cd43c15db6bb2273b822497e0cca501
Alejandro17Cast <luisalejandrocasta3@gmail.com>
2024-05-26 10:24:29 -0600

    .

--- src/Camion.java
+++ src/Camion.java
@@ -23,11 +23,18 @@ public class Camion extends vehiculoCarga {
         System.out.println("Numero de Ejes: " + numEjes);
 
     }
-
-<<<<<<< HEAD
-
+  @Override
+     public void realizarMantenmiento(){
+        super.realizarMantenmiento();
+        System.out.println("1. Limpieza del motor");
+        System.out.println("2. Cambio de llantas");
+        System.out.println("3. Arreglos de tricos ");
     }
 
-=======
+
+    public void costoMantenimiento(double kmrecorridos, double costoporkm){
+  double CostoMantenimiento=kmrecorridos*costoporkm; 
+  System.out.println("El costo total por el mantenimiento es: "+CostoMantenimiento);  
+    }
+
 }
->>>>>>> QA
